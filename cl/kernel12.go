6fd9002951f55d7d2e83802aa553b42081cf7590
Xuanyao Fong <xuanyao.fong@gmail.com>
2015-11-28 12:54:13 +0800

    Revert some changes to kernel12.go

--- cl/kernel12.go
+++ cl/kernel12.go
@@ -13,13 +13,8 @@ import "unsafe"
 func (k *Kernel) ArgName(index int) (string, error) {
 	var strC [1024]byte
 	var strN C.size_t
-	// get the size of output
-	err := C.clGetKernelArgInfo(k.clKernel, C.cl_uint(index), C.CL_KERNEL_ARG_NAME, 0, unsafe.Pointer(&strC[0]), &strN)
-	if err != C.CL_SUCCESS {
-		return "abcd", toError(err)
-	}
-	if err = C.clGetKernelArgInfo(k.clKernel, C.cl_uint(index), C.CL_KERNEL_ARG_NAME, strN, unsafe.Pointer(&strC[0]), nil); err != C.CL_SUCCESS {
-		return "123", toError(err)
+	if err := C.clGetKernelArgInfo(k.clKernel, C.cl_uint(index), C.CL_KERNEL_ARG_NAME, 1024, unsafe.Pointer(&strC[0]), &strN); err != C.CL_SUCCESS {
+		return "", toError(err)
 	}
 	return string(strC[:strN]), nil
 }
